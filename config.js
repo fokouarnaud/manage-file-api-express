92aa82442ec409874d1ff99fa13908e27ab025bd
fokouarnaud <fokouarnaud@gmail.com>
2022-06-21 14:15:39 +0200

    update

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
+    host: "localhost",
+    user: "root",
+    password: "fokou2014",
+    database: "api_doc_db",
+    connectionLimit : 10,
+  },
+  dbo: {
+    /* don't expose password or any sensitive info, done only for demo */
     host: "us-cdbr-east-05.cleardb.net",
     user: "b7baa28aa75f16",
     password: "8cbff95e",
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const multer = require('multer');
-
+const { s3Uploadv2 } = require("./s3Service");
 const port = 3000;
 const documentsRouter = require("./routes/documents");
 
-
-
 app.use(cors());
-app.use(express.static('public'));
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -17,26 +15,71 @@ app.use(
   })
 );
 
+// custom filename
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, 'public')
-  },
-  filename: (req, file, cb) => {
-      cb(null, Date.now() + '-' + file.originalname)
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     const { originalname } = file;
+//     cb(null, `${uuid()}-${originalname}`);
+//   },
+// });
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
   }
-});
+};
 
-const upload = multer({storage}).array('file');
 
-app.post('/upload', (req, res,next) => {
-    upload(req, res, (err) => {
-        if (err) {
-            return next(err);
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1000000000, files: 1 },
+}).array("file");
+
+
+app.post("/upload", async (req, res) => {
+  try {
+    upload(req, res, async function (error) {
+
+      if (error instanceof multer.MulterError) {
+        if (error.code === "LIMIT_FILE_SIZE") {
+          return res.status(400).json({
+            message: "file is too large",
+          });
         }
 
-        return res.send(req.files);
-    })
+        if (error.code === "LIMIT_FILE_COUNT") {
+          return res.status(400).json({
+            message: "File limit reached",
+          });
+        }
+
+        if (error.code === "LIMIT_UNEXPECTED_FILE") {
+          return res.status(400).json({
+            message: "File must be an pdf",
+          });
+        }
+      }
+
+      const results = await s3Uploadv2(req.files);
+
+      //save result info to database
+
+      return res.json({ status: "success",results });
+
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get("/", (req, res) => {
@@ -55,6 +98,8 @@ app.use((err, req, res, next) => {
 
   return;
 });
+
+
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
