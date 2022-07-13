2fb2190f8589374b5fc51be9dbb89300d0d12b04
fokouarnaud <fokouarnaud@gmail.com>
2022-07-13 05:40:54 +0100

    Update documents.js

--- routes/documents.js
+++ routes/documents.js
@@ -89,7 +89,7 @@ router.post("/", async function (req, res, next) {
 
       // upload file on remote storage
       const results = await s3Uploadv2(req.files);
-      const document = { ...req.body, source_doc: results[0]?.Key, annee_soutenance: "2022-02-07" };
+      const document = { ...req.body, source_doc: results[0]?.Key };
       //save result info to database
       res.json(await documents.create(document));
 
